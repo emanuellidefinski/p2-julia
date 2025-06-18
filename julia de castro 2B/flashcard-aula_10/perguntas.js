1cc044e31228ef54768e8d892642f8e588a9e107
emanuellidefinski <emanuelli.definski@escola.pr.gov.br>
2025-06-18 12:14:23 -0300

    Add files via upload

---
+++ julia de castro 2B/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'sobre comida',
+    'qual a comida tipica da Italia?',
+    'a comida tipica da Italia é pizza'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'qual o filme q o menino tem uma cicatriz de raio na testa?',
+    'Harry Potter'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa no português a palavra crazy?',
+    'significa louco/doido'
+)
